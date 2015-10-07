d7899e04e0ebfd98dab109c08b387a7b72091a09
pyskell <ajlusardi@gmail.com>
2015-10-07 11:50:10 -0400

    ehh

--- taskbar_icon.py
+++ taskbar_icon.py
@@ -4,6 +4,8 @@ import time
 import wx
 import wx.lib.agw.toasterbox as TB
 
+from wxAnyThread import anythread
+
 from main import main
 
 # from wx import TaskBarIcon
@@ -13,14 +15,20 @@ class ToasterFrame(wx.Frame):
 
     wx.Frame.__init__(self, parent, -1)
 
-    toaster = TB.ToasterBox(self, tbstyle=TB.TB_SIMPLE)
+    self.toaster = TB.ToasterBox(self, tbstyle=TB.TB_SIMPLE)
 
-    toaster.SetPopupPauseTime(5000)
-    toaster.SetPopupPositionByInt(3)
-    toaster.SetPopupSize((100, 100))
-    toaster.SetPopupText("You're slouching.")
+    self.toaster.SetPopupPauseTime(5000)
+    self.toaster.SetPopupPositionByInt(3)
+    self.toaster.SetPopupSize((100, 100))
+    self.toaster.SetPopupBackgroundColour(wx.Colour(0,0,0))
+    self.toaster.SetPopupTextColour(wx.Colour(255,255,255))
+    self.toaster.SetPopupText("You're slouching.")
 
-    wx.CallLater(1000, toaster.Play)
+  @anythread
+  def show_popup(self):
+    wx.CallLater(1000, self.Show(False)) # Hides the main app window?
+    wx.CallLater(1000, self.toaster.Play)
+    # wx.CallLater(2000, toaster.CleanList)
 
 def start_thread(func, *args):
     thread = threading.Thread(target=func, args=args)
@@ -28,22 +36,23 @@ def start_thread(func, *args):
     thread.start()
 
 def check_slouching():
-
   while True:
     slouching = main()
 
     if slouching:
       alert = ToasterFrame(None)
-      app.SetTopWindow(alert)
-      wx.CallLater(2000, alert.Show(False)) # Hides the main app window?
+      alert.show_popup()
+      # wx.CallLater(1000, alert.toaster.Play)
       # wx.CallLater(1000, alert.Destroy)
       # wx.CallLater(2000, alert.Destroy)
     
-    time.sleep(20)
+    time.sleep(60)
 
 app = wx.App(0)
 icon = wx.TaskBarIcon()
 icon.SetIcon(wx.Icon('favicon_32.bmp', type=wx.BITMAP_TYPE_ANY))
+
+# app.SetTopWindow(alert)
 
 start_thread(check_slouching)
 
