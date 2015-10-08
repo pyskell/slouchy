9c58738234363be59efbc825c612a822e8dce5ee
pyskell <ajlusardi@gmail.com>
2015-10-08 16:47:07 -0400

    added a disclaimer, cause people

--- README.md
+++ README.md
@@ -16,3 +16,6 @@ If it complains about multiple faces run `main.py` and you should get an output 
 
 # License
 This software is released under the GNU GPL version 3 except for the `haarscascade_frontalface_default.xml` file which is released under the Intel License Agreement For Open Source Computer Vision Library
+
+# Disclaimer
+This software is not intended to diagnosis, cure, or prevent diseases in any way. No warranties are made or implied for its efficacy. It's simply a little program the author wanted and decided to share.
--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from configobj import ConfigObj
 
-from main import calculate_c_squared, take_picture, detect_face
+from main import video_device, calculate_c_squared, take_picture, detect_face
 
 # Set initial values
 def setup():
   config = ConfigObj('slouchy.ini')
 
-  #video_device can be an int or a string, so try int, and if not assume string
-  try:
-    video_device = int(config['MAIN']['video_device'])
-  except ValueError:
-    video_device = str(config['MAIN']['video_device'])
+  # #video_device can be an int or a string, so try int, and if not assume string
+  # try:
+  #   video_device = int(config['MAIN']['video_device'])
+  # except ValueError:
+  #   video_device = str(config['MAIN']['video_device'])
 
-  maybe_image = take_picture(video_device)
-  maybe_face = detect_face(maybe_image)
+  maybe_image     = take_picture(video_device)
+  maybe_face      = detect_face(maybe_image)
   maybe_c_squared = calculate_c_squared(maybe_face)
 
   if maybe_c_squared.success:
@@ -25,4 +25,5 @@ def setup():
 
   config.write()
 
-setup()
+if __name__ == '__main__':
+  setup()
